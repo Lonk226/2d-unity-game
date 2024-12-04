62a932196e9ec91aed3113cc42881d50d99a027d
NoahLivingston123 <ng_livingston@icloud.com>
2024-12-04 09:13:44 -0500

    hiiiii

---
+++ 2d game/Assets/PlayerMovements.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    void Update(){
+
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+   
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
