2ea01822332dcd37a8cac2c2a3580813457131fc
NoahLivingston123 <ng_livingston@icloud.com>
2024-12-04 10:01:31 -0500

    Hi

--- 2d game/Assets/PlayerMovements.cs
+++ 2d game/Assets/PlayerMovements.cs
@@ -7,18 +7,24 @@ public class PlayerMovements : MonoBehaviour
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
+    bool jump = false;
 
     void Update(){
 
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-   
 
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
         
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
